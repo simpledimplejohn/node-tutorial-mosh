ab01ac7b38d09a5d8cfcdc7c92ac86764f267ada
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-03 14:25:28 -0700

    File Systems module example and notes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//using the file system module
+
 const fs = require('fs');
 
+// syncronously--do NOT do this
 // const files = fs.readdirSync('./');
 // console.log(files);
 
-fs.readdir('$', function(err, files) {
+//asyncronous calls a function with an error object or the file
+fs.readdir('./', function(err, files) {
   if(err) console.log('Error', err);
   else console.log('Result', files);
 });
