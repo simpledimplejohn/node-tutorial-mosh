04efdc1cee592285028c4adcad50187acbabac1c
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-03 14:53:40 -0700

    extending event emitter

--- app.js
+++ app.js
@@ -4,19 +4,17 @@
 
 //EventEmitter is a class that contains the event
 const EventEmitter = require('events')
-//second one is now an object
-const emitter = new EventEmitter();
+
+//now we access a class with require
+//this class now extends EventEmitter
+const Logger = require('./logger');
+//make a new instance of this object
+const logger = new Logger();
 
 // Register a listener
 // must be called before the emiter
-emitter.on('messageLogged', (arg) => {
+logger.on('messageLogged', (arg) => {
   console.log('listener called', arg);
 })
 
-
-//emit = make a noise or produce something
-//raise an event
-// add an object with paramaters about the message
-emitter.emit('messageLogged', {id: 1, url: 'http://', data: 'message'});
-
-//raise: logging send (data: message)
+logger.log('message');
---
+++ logger.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('events')
+
+var url = 'http://mylogger.io/log';
+
+//es6 lets us use extends that takes everything from the EventEmitter and adds it
+class Logger extends EventEmitter{
+
+  log(message) {
+    // send an http request
+    console.log(message);
+    //now raising the event with this--which accesses the object info
+    this.emit('messageLogged', {id: 1, url: 'http://', data: 'message'});
+  }
+}
+
+
+
+module.exports = Logger;
